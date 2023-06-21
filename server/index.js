9b4723815d882c45da4cda490e821467d9622f18
SanghaviHarshPankajkumar <harshpanakajsanghai@gmail.com>
2023-06-21 18:41:33 +0530

    dotenv package is added

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 import { getUser, getUsers, removeUser, addUser } from './users.js';
+import dotenv from 'dotenv'
+
+dotenv.config()
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
