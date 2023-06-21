6b06256fb911fb299db781f542a251755fc31ee5
SanghaviHarshPankajkumar <harshpanakajsanghai@gmail.com>
2023-06-21 18:30:23 +0530

    commit before pushing on github

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-/* 
-    ***************reamining Tasks***************
-    - chats layout :- done...
-    - add navbarui :-done...
-    - copy button functionality :- Done...
-    - fix  refresh disconnects room :- Done...
-    - alert messages :- done...
-    
-*/
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
